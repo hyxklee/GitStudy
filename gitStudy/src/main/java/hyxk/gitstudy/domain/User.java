ab0ce04f96cafacf118bcd7bb6ee92ffa3c20dc5
hyuna.cho <02hyuna@gmail.com>
2024-05-19 23:57:12 +0900

    third commit

--- gitStudy/src/main/java/hyxk/gitstudy/domain/User.java
+++ gitStudy/src/main/java/hyxk/gitstudy/domain/User.java
@@ -4,6 +4,6 @@ public class User {
     public String main(){
         System.out.println("이강혁 아저씨");
         System.out.println("아이코스 뻑뻑 피는 이강혁");
-
+        System.out.println("이사 온 사람들을 걱정하는 이강혁");
     }
 }
