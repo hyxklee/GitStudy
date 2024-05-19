86b97a19a49bf065bbe5151941472a42fd8bff23
hyxklee <ewgt1234@naver.com>
2024-05-20 00:09:08 +0900

    refactor(User): main 메소드 수정

--- gitStudy/src/main/java/hyxk/gitstudy/domain/User.java
+++ gitStudy/src/main/java/hyxk/gitstudy/domain/User.java
@@ -5,6 +5,6 @@ public class User {
         System.out.println("이강혁 아저씨");
         System.out.println("아이코스 뻑뻑 피는 이강혁");
         System.out.println("이사 온 사람들을 걱정하는 이강혁");
-        System.out.println("홍대가서 대리운전하고온 조현아");
+        System.out.println("홍대가서 대리운전하고온 조현아!");
     }
 }
