d3f825d1128cbe4fba6de4df89280318771a3116
hyuna.cho <02hyuna@gmail.com>
2024-05-21 17:33:30 +0900

    feat: 업데이트 테스트

--- gitStudy/src/main/java/hyxk/gitstudy/domain/User.java
+++ gitStudy/src/main/java/hyxk/gitstudy/domain/User.java
@@ -5,7 +5,6 @@ public class User {
         System.out.println("이강혁 아저씨");
         System.out.println("아이코스 뻑뻑 피는 이강혁");
         System.out.println("이사 온 사람들을 걱정하는 이강혁");
-        System.out.println("홍대가서 운전연습만 하고 돌아온 조현아");
-        System.out.println("월요일부터 늦잠잔 조현아");
+        System.out.println("바지가 늘어나서 불만인 이강혁");
     }
 }
